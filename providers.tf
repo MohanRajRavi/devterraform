74dcd35b75b5e9889eadf60c1b7afea56097548e
root <mohan_ravi3@yahoo.co.in>
2018-11-15 07:22:31 -0400

    updated variables and providers file

---
+++ providers.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+ aws_key_file="var.aws_key_direcory"
+ region="var.aws_region"
+}
---
+++ varibales.tf
@@ -0,0 +1,9 @@
+###### AWS Providers Config ####
+
+Variable "aws_key_direcory" {
+	default="/root/.aws/credentials (END)"
+}
+
+variable "aws_region" {
+	default="us-east-1"
+}
